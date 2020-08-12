60e0b3701712cb3719bbebd1c69da7a70cdc0665
James Forster <jamesalexforster@gmail.com>
2020-08-12 15:30:25 +0100

    wrote test to ensure quality degrades twice as fast after expiry for standard items

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -16,6 +16,13 @@ describe GildedRose do
       expect(items[0].quality).to eq(19)
     end
 
+    it "will degrade standard items twice as fast after expiry" do
+      items = [Item.new(name="+5 Dexterity Vest", sell_in=0, quality=20)]
+      GildedRose.new(items).update_quality
+      expect(items[0].sell_in).to eq(-1)
+      expect(items[0].quality).to eq(18)
+    end
+  
   end
 
 end
