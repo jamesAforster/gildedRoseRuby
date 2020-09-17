b7c57677761b39c8fbebed97ad83f508d4ad8dc4
James Forster <jamesalexforster@gmail.com>
2020-09-17 09:18:30 +0100

    pushing to github

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -2,55 +2,24 @@ class GildedRose
 
   def initialize(items)
     @items = items
+    @standard_items = ["+5 Dexterity Vest", "Elixir of the Mongoose"]
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+      if @standard_items.include?(item) 
+        puts "Item is found in standard_item array"
+        daily_update(item)
       end
     end
   end
+
+  def daily_update(item)
+    item.sell_in -= 1
+    puts "Item has been passed to daily update"
+    item.quality -= 1
+  end
+
 end
 
 class Item
